5ac0480df03ae847c9aac0e74c950c8448fc2b3f
Ty Le <anhtuan.n.le@gmail.com>
2017-03-23 14:14:55 -0400

    Initial commit

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,18 @@
+const React 	= require("react");
+
+
+var Timer = React.createClass({
+	render: function () {
+		return (
+			<h5>Countdown Component</h5>
+		)
+	}
+})
+
+
+
+
+
+
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,18 @@
+const React 	= require("react");
+
+
+var Timer = React.createClass({
+	render: function () {
+		return (
+			<h5>Timer Component</h5>
+		)
+	}
+})
+
+
+
+
+
+
+
+module.exports = Timer;
